4bf02e91c7db62a481cb2effd0f8c315fd2d9ef1
DSander <dsander@beeline.kz>
2026-01-20 20:05:13 +0500

    KS-1-3 add Gagarin's fly

---
+++ src/main/kotlin/lesson_1/Lesson1_Task3.kt
@@ -0,0 +1,21 @@
+package sprint.lessons.lesson_1
+
+fun main () {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    print(hour)
+    print(":")
+    println(minute)
+
+    println("$hour:$minute") // возможная вероятность
+
+}
