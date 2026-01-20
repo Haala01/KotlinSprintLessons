9714cae47f90bcfcb50084df69106ca8495fcafb
DSander <dsander@beeline.kz>
2026-01-20 19:40:19 +0500

    KS-1-1 add shop orders

---
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -0,0 +1,10 @@
+package sprint.lessons.lesson1_task1
+
+fun main () {
+
+    var numberOfOrders: Int = 75
+    val thxMessage: String = "Spasibo za pokupki!"
+
+    println(numberOfOrders)
+    println(thxMessage)
+}
