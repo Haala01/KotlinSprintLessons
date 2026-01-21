5edb4f27c175b16ee4caa5349dfadf56fffaa9f9
DSander <dsander@beeline.kz>
2026-01-21 19:48:24 +0500

    KS-1-5 resolve comments

--- src/main/kotlin/lesson_1/Lesson1_Task5.kt
+++ src/main/kotlin/lesson_1/Lesson1_Task5.kt
@@ -1,12 +1,16 @@
 package sprint.lessons.lesson_1
 
+const val SECONDS_IN_MINUTE = 60
+const val MINUTES_IN_HOUR = 60
+const val SECONDS_IN_HOUR = SECONDS_IN_MINUTE * MINUTES_IN_HOUR
+
 fun main () {
 
-
     val totalSeconds = 6480
-    var hours = totalSeconds / 3600
-    var minutes = (totalSeconds % 3600) / 60
-    var seconds = totalSeconds % 60
+
+    val hours: Int = totalSeconds / SECONDS_IN_HOUR
+    val minutes: Int = (totalSeconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val seconds: Int = totalSeconds % SECONDS_IN_MINUTE
 
     println("%02d:%02d:%02d" .format (hours, minutes, seconds))
 
