80d923c73acf23383faab6136a33c95e6ea43966
DSander <dsander@beeline.kz>
2026-01-23 12:09:45 +0500

    KS-3-2 Tanya task

---
+++ src/main/kotlin/lesson_3/Lesson3_Task2.kt
@@ -0,0 +1,18 @@
+package sprint.lessons.lesson_3
+
+fun main () {
+
+    val firstName = "Татьяна"
+    val patronymic = "Сергеевна"
+
+    var lastName = "Андреева"
+    var age = 20
+
+    println("[$lastName,$firstName,$patronymic,$age]")
+
+    lastName = "Сидорова"
+    age = 22
+
+    println("[$lastName,$firstName,$patronymic,$age]")
+
+}
