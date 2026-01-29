7fa981ad59a2c35b57ade422dd5c9e9cbe68c12d
DSander <dsander@beeline.kz>
2026-01-29 19:52:52 +0500

    KS-4-3 resolve comments

--- src/main/kotlin/lesson_4/Lesson4_Task3.kt
+++ src/main/kotlin/lesson_4/Lesson4_Task3.kt
@@ -1,12 +1,18 @@
 package sprint.lessons.lesson_4
 
+const val TARGET_HUMIDITY = 20
+const val FORBIDDEN_SEASON = "зима"
+
 fun main () {
 
     val isSunny = true
     val isAwningOpen = true
-    val humidity = 20
+    val currentHumidity = 20
     val currentSeason = "зима"
-    val conditionsMet = isSunny && isAwningOpen && (humidity == 20) && (currentSeason != "зима")
+    val conditionsMet = isSunny
+            && isAwningOpen
+            && (currentHumidity == TARGET_HUMIDITY)
+            && (currentSeason != FORBIDDEN_SEASON)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $conditionsMet")
 
